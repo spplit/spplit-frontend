36d5434e9e7c5a658b7f2a55461e3553671b6935
Sanghyo <sanghyoboy@naver.com>
2021-06-29 11:40:54 +0900

    qr floating and scroll up floating display

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -6,14 +6,14 @@ import QRFloating from '../components/QRFloating';
 import QRModal from '../components/QRModal';
 
 function Main() {
-    const [qrModalOn, setQrModalOn] = useState(true);
+    const [qrModalOn, setQrModalOn] = useState(false);
 
     return (
         <div>
             <Header />
-            <QRFloating bottom={40} right={40} setData={setQrModalOn} data={qrModalOn} />
-            <QRModal modalOn={qrModalOn} bottom={40} right={40} />
-            <ScrollupFloating bottom={80} right={40} />
+            <QRFloating bottom={40} right={100} setData={setQrModalOn} data={qrModalOn} />
+            <QRModal modalOn={qrModalOn} bottom={40} right={100} />
+            <ScrollupFloating bottom={50} right={40} />
         </div>
     )
 }
