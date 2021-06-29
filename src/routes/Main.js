364ce4743f1401d35fea6cd6b9f44cf5d7474dd6
Sanghyo <sanghyoboy@naver.com>
2021-06-29 10:26:33 +0900

    make floating button for scrolling up to top

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import ScrollupFloating from '../components/ScrollupFloating';
 
 function Main() {
     return (
         <div>
             <Header />
+            <ScrollupFloating bottom={80} right={40} />
         </div>
     )
 }
