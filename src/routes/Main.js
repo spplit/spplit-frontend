771dc5fe0899ae8d73cf2542d4b82c9d43022193
Sukyeong Park <zztnrudzz123433@gmail.com>
2021-06-29 23:32:59 +0900

    NavigationBar

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import NavigationBar from '../components/NavigationBar';
 
 function Main() {
     return (
         <div>
             <Header />
+            <NavigationBar />
         </div>
     )
 }
