3d9a828431fad4e70f7339a264f765b775ced40e
jeongbeom <sjb990221@gmail.com>
2021-07-02 15:34:27 +0900

    update feat#28

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -11,21 +11,11 @@ import Search from '../components/Search';
 
 function Main() {
     const [qrModalOn, setQrModalOn] = useState(false);
-    const [cardList, setCardList] = useState([]);
-
-    useEffect(() => {
-        axios.get('http://localhost:8080/cards').then((res) => {
-            setCardList(res.data)
-
-        })
-    }, [])
 
     return (
         <div>
             <Header />
-
             <Search />
-            <List cards={cardList} />
             <QRFloating bottom={55} right={40} setData={setQrModalOn} data={qrModalOn} />
             <QRModal modalOn={qrModalOn} bottom={55} right={40} />
             {/* <ScrollupFloating bottom={50} right={40} /> */}
